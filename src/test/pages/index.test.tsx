265be2f6e02140ee1c49044d11083e069602ad0e
Charles X. Morrissey <me@charles-x.com>
2021-07-09 23:54:28 -0400

    add placeholder page test

--- src/test/pages/index.test.tsx
+++ src/test/pages/index.test.tsx
@@ -5,11 +5,5 @@ import IndexPage from 'pages/index'
 describe('IndexPage', () => {
   it('should the index page', () => {
     render(<IndexPage />)
-
-    // const heading = screen.getByText(/Main content/i)
-
-    // We can only use toBeInTheDocument because it was imported
-    // in the jest.setup.js and configured in jest.config.js
-    // expect(heading).toBeInTheDocument()
   })
 })
