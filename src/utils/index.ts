bda747aec27b4e3314ed09f516a3f11a862efc8a
Charles X. Morrissey <me@charles-x.com>
2021-12-17 12:26:01 -0500

    DRY up setBackground

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,10 @@
  * @param {HTMLElement} element The element to set inline styles on.
  */
 export const setBackground = (element: HTMLElement | null) => {
-  const random = (min: number, max: number) =>
+  const total = 4
+  const random = (min: number, max: number): number =>
     Math.floor(Math.random() * (max - min) + min)
-  const randomColor = () => {
+  const randomColor = (): string => {
     const h = random(0, 360)
     const s = random(50, 100)
     const l = random(50, 100)
@@ -14,8 +15,7 @@ export const setBackground = (element: HTMLElement | null) => {
     return `hsl(${h}, ${s}%, ${l}%)`
   }
 
-  element?.style.setProperty('--color-1', randomColor())
-  element?.style.setProperty('--color-2', randomColor())
-  element?.style.setProperty('--color-3', randomColor())
-  element?.style.setProperty('--color-4', randomColor())
+  for (let i = 0; i < total; i++) {
+    element?.style.setProperty(`--color-${i + 1}`, randomColor())
+  }
 }
