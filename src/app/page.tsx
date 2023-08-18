f3b5eabbc686e5964417de6dbe56dd9a5da3051b
Charles X. Morrissey <me@charles-x.com>
2023-08-17 22:37:46 -0400

    Move header content to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@
 
 import { useEffect, useRef } from 'react'
 
+import { Header } from 'components/Header'
 import { Social } from 'components/Social'
 import { APP_DATA, SOCIAL_DATA } from 'constant'
 import { setBackgroundStyles } from 'utils'
-
-const { description, name } = APP_DATA
 
 const Home = () => {
   const backgroundRef = useRef<HTMLElement>(null)
@@ -18,18 +17,7 @@ const Home = () => {
       className='animate-bg-init flex flex-col min-h-screen p-6 md:px-8'
       ref={backgroundRef}
     >
-      <header className='max-w-lg'>
-        <h1 className='font-semibold text-3xl md:text-5xl tracking-tighter md:tracking-tight mb-3 md:mb-4'>
-          {name}
-        </h1>
-
-        <p className='text-lg md:text-2xl leading-6 tracking-tight mb-3 md:mb-5'>
-          {description}
-        </p>
-      </header>
-
-      <h2 className='font-semibold'>Get in touch</h2>
-
+      <Header {...APP_DATA} />
       <Social data={SOCIAL_DATA} />
     </main>
   )
--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -5,18 +5,22 @@ interface SocialProps {
 }
 
 export const Social = ({ data }: SocialProps) => (
-  <div className='flex items-center ml-[-0.5rem] md:gap-1'>
-    {data.map(({ Icon, link, name, options }: LinkData, index) => (
-      <a
-        className='p-2 hover:text-slate-700 focus:outline-none focus:ring-1 focus:ring-slate-50/50 transition-[box-shadow,color] rounded-md'
-        href={link}
-        key={`social-item-${index}`}
-        {...options}
-      >
-        <span className='sr-only'>{name}</span>
+  <>
+    <h2 className='font-semibold'>Get in touch</h2>
 
-        <Icon aria-hidden className='w-6 h-6 fill-current shrink-0' />
-      </a>
-    ))}
-  </div>
+    <div className='flex items-center ml-[-0.5rem] md:gap-1'>
+      {data.map(({ Icon, link, name, options }: LinkData, index) => (
+        <a
+          className='p-2 hover:text-slate-700 focus:outline-none focus:ring-1 focus:ring-slate-50/50 transition-[box-shadow,color] rounded-md'
+          href={link}
+          key={`social-item-${index}`}
+          {...options}
+        >
+          <span className='sr-only'>{name}</span>
+
+          <Icon aria-hidden className='w-6 h-6 fill-current shrink-0' />
+        </a>
+      ))}
+    </div>
+  </>
 )
