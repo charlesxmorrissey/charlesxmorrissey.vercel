46e80f7b117a4f405fc65baac69dbbb917eae121
Charles X. Morrissey <me@charles-x.com>
2025-12-23 20:00:57 -0500

    feat(turbopack): update to use turbopack

--- next.config.ts
+++ next.config.ts
@@ -5,50 +5,13 @@ const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
 
-  turbopack: {},
-
-  webpack: (config) => {
-    // Configures webpack to handle SVG files with SVGR.
-    // SVGR optimizes and transforms SVG files into React components.
-    // See https://react-svgr.com/docs/next/
-
-    // Grab the existing rule that handles SVG imports.
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.('.svg'),
-    )
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        resourceQuery: /url/, // *.svg?url
-        test: /\.svg$/i,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        as: '*.js',
+        loaders: ['@svgr/webpack'],
       },
-
-      // Convert all other *.svg imports to React components.
-      {
-        issuer: fileLoaderRule.issuer,
-        // Exclude if *.svg?url
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        test: /\.svg$/i,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              prettier: false,
-              svgo: true,
-              titleProp: true,
-            },
-          },
-        ],
-      },
-    )
-
-    // Modify the file loader rule to ignore *.svg,
-    // since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
+    },
   },
 }
 
