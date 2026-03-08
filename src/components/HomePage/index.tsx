a0161dae36731c46b660d68b471cdc6615fcbe4a
Charles X. Morrissey <me@charles-x.com>
2026-03-08 17:18:23 -0400

    refactor: remove redundant useCallback in HomePage
    React Compiler auto-memoizes, making the manual useCallback unnecessary.

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useCallback } from 'react'
 import { Header } from 'components/Header'
 import { Social } from 'components/Social'
 import { APP_DATA, SOCIAL_DATA } from 'constant'
@@ -9,11 +8,11 @@ import { setBackgroundStyles } from 'utils'
 import styles from './HomePage.module.css'
 
 export const HomePage = () => {
-  const backgroundRef = useCallback((element: HTMLElement | null) => {
+  const backgroundRef = (element: HTMLElement | null) => {
     if (element) {
       setBackgroundStyles(element)
     }
-  }, [])
+  }
 
   return (
     <main className={styles.wrapper} ref={backgroundRef}>
